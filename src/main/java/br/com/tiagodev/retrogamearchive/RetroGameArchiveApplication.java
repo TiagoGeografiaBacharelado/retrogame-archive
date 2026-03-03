3b8b9118c56c6be7085c74cceaa650a01d8e840d
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2026-03-03 14:34:52 -0300

    Update
     "Initial project structure generated by Spring Initializr"

---
+++ src/main/java/br/com/tiagodev/retrogamearchive/RetroGameArchiveApplication.java
@@ -0,0 +1,13 @@
+package br.com.tiagodev.retrogamearchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetroGameArchiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetroGameArchiveApplication.class, args);
+	}
+
+}
