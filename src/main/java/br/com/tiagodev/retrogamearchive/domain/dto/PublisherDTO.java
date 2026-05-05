bd5d0d62dd2c5592a68285fd51dcd99e7074de08
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2026-05-05 15:54:38 -0300

    Create PublisherDTO
    feat: add PublisherDTO.

---
+++ src/main/java/br/com/tiagodev/retrogamearchive/domain/dto/PublisherDTO.java
@@ -0,0 +1,18 @@
+package br.com.tiagodev.retrogamearchive.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PublisherDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+    
+}
