c4397a14a4bd2587783770e7df2f9f1c9d911408
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2026-05-05 16:46:45 -0300

    Create FranchiseDTO
    Feat: add FranchiseDTO

---
+++ src/main/java/br/com/tiagodev/retrogamearchive/domain/dto/FranchiseDTO.java
@@ -0,0 +1,18 @@
+package br.com.tiagodev.retrogamearchive.domain.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class FranchiseDTO {
+
+    private Long id;
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+}
