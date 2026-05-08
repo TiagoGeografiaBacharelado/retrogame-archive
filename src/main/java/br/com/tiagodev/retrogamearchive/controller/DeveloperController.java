df142415ab4be269b428311077b9cb7185eed582
TiagoCarneiroDoNascimento <tiago.tcan@gmail.com>
2026-05-08 19:49:07 -0300

    Update
    Fix: PublisherController, FranchiseController, DeveloperController chamadas no plural.

--- src/main/java/br/com/tiagodev/retrogamearchive/controller/DeveloperController.java
+++ src/main/java/br/com/tiagodev/retrogamearchive/controller/DeveloperController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/developer")
+@RequestMapping("/developers")
 @RequiredArgsConstructor
 public class DeveloperController {
 
